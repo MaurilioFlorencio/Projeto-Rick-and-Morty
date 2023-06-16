b12715b5a15a5188bafb41f2fe1037d7b6e62662
Maurilio <maurilio.f.d.silva@gmail.com>
2023-06-15 21:39:53 -0300

    projeto basico

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const main = document.querySelector("main")
+
+
 fetch("https://rickandmortyapi.com/api/character")
 .then (res => res.json())
-.then (res => {
-    res.forEach(repo => {
+.then(res => {
+    res.results.forEach(perso => {
+        main.innerHTML += 
+        `
+        <div class="card">
+            <span class="nome">${perso.name}</span>
+            <img class= "imagem-card" src="${perso.image}" alt="${perso.image}">
+            <span class="origem">${perso.origin.name}</span>
+        </div>
+        `   
+
     });
+    console.log(res.results)
 })
