5fffe68f213293c57a05b2b261a2eba008a19a53
Maurílio <maurilio.f.d.silva@gmail.com>
2023-06-19 20:09:57 -0300

    redirecionamento por card

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const main = document.querySelector("main")
 
-
 fetch("https://rickandmortyapi.com/api/character")
 .then (res => res.json())
 .then(res => {
     res.results.forEach(perso => {
         main.innerHTML += 
         `
-        <div class="card">
+        <div class="card" onclick="redirecionar()">
             <span class="nome">${perso.name}</span>
             <img class= "imagem-card" src="${perso.image}" alt="${perso.image}">
             <span class="origem">${perso.origin.name}</span>
         </div>
         `   
-
     });
     console.log(res.results)
 })
+
+function redirecionar (){
+    window.location.href = "info.html";
+}
